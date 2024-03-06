093b2f1d6161ced9b9bf24b700cd842e8aef7080
Tobias Ericsson <tobias.ericsson_c@sandvik.com>
2024-03-06 11:20:33 +0100

    assets

--- app/js/dom.js
+++ app/js/dom.js
@@ -1,29 +1,35 @@
 var Dom = (function () {
   // Get the content div
   var contentDiv = document.getElementById('content')
+  var footer = document.getElementById('footer')
 
   function pageToHTML (jsonData) {
     var html = '<div>'
 
-    var header = '<h2>' + jsonData.title + '</h2>'
-    var content = '<p>' + jsonData.question + '</p>'
+    //var header = '<h2>' + jsonData.title + '</h2>'
 
-    html += header + content
+    jsonData.question.forEach(line => (html += '<p>' + line + '</p>'))
+
+    //var content = '<p>' + jsonData.question + '</p>'
+
+    //html += content
 
     html += '</div>'
     contentDiv.innerHTML = html
+    footer.innerHTML = jsonData.title
   }
 
   function questionToHTML (jsonData) {
     var html = '<div>'
 
-    var header = '<h2>' + jsonData.title + '</h2>'
+    //var header = '<h2>' + jsonData.title + '</h2>'
     var content = '<p>' + jsonData.question + '</p>'
 
     html += header + content
 
     html += '</div>'
     contentDiv.innerHTML = html
+    footer.innerHTML = jsonData.title
   }
 
   return {
---
+++ app/js/game-state.js
@@ -0,0 +1,58 @@
+class GameState {
+  constructor () {
+    this.outputElement = document.getElementById('output')
+    this.inputElement = document.getElementById('input')
+    this.commands = []
+    this.currentCommandIndex = -1
+    this.gameContent = null
+  }
+
+  start () {
+    this.print('Welcome to the Text Adventure Game!')
+    this.print("Type 'help' for available commands.")
+    this.inputElement.focus()
+  }
+
+  print (message) {
+    this.outputElement.innerHTML += `<p>${message}</p>`
+  }
+
+  clearInput () {
+    this.inputElement.value = ''
+  }
+
+  processCommand (command) {
+    this.print(`> ${command}`)
+    // Process the command based on game content
+    // Example: this.gameContent.processCommand(command);
+    this.clearInput()
+  }
+
+  handleInput (e) {
+    if (e.key === 'Enter') {
+      const command = this.inputElement.value.trim()
+      if (command !== '') {
+        this.commands.push(command)
+        this.currentCommandIndex = this.commands.length
+        this.processCommand(command)
+      }
+    } else if (e.key === 'ArrowUp') {
+      if (this.currentCommandIndex > 0) {
+        this.currentCommandIndex--
+        this.inputElement.value = this.commands[this.currentCommandIndex]
+      }
+    } else if (e.key === 'ArrowDown') {
+      if (this.currentCommandIndex < this.commands.length - 1) {
+        this.currentCommandIndex++
+        this.inputElement.value = this.commands[this.currentCommandIndex]
+      } else {
+        this.currentCommandIndex = this.commands.length
+        this.clearInput()
+      }
+    }
+  }
+}
+
+function submitCommand () {
+  game.processCommand(document.getElementById('input').value)
+}
